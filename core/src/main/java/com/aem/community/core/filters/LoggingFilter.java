ee39377a6ff2985919a2f4038f08eb9122b32a77
Nazir <nazir.samer@verticurl.com>
2019-12-16 15:10:41 +0530

    Commit for revision test

--- core/src/main/java/com/aem/community/core/filters/LoggingFilter.java
+++ core/src/main/java/com/aem/community/core/filters/LoggingFilter.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2015 Adobe Systems Incorporated ohk test 1.7
+ *  Copyright 2015 Adobe Systems Incorporated Revison branch
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
